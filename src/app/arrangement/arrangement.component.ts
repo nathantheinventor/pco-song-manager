ebd2668ab6f395ae94a627fe445fe3fbd378f7ca
Nathan Collins <nathantheinventor@gmail.com>
2019-11-09 11:03:07 -0500

    Starting on displaying arrangements

--- src/app/arrangement/arrangement.component.ts
+++ src/app/arrangement/arrangement.component.ts
@@ -7,13 +7,36 @@ import { ApiService } from '../api.service';
   styleUrls: ['./arrangement.component.scss']
 })
 export class ArrangementComponent implements OnInit {
-  @Input() songId;
-  @Input() arrangementId;
+  @Input() songName: string;
+  @Input() songId: number;
+  @Input() arrangementId: number;
+  arrangement: any;
+  lyricVerses = {};
+  editRaw = false;
 
   constructor(private api: ApiService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.arrangement = await this.api.get_arrangement(this.songId, this.arrangementId);
 
+    let currentVerseName = '';
+    let currentVerse = '';
+    for (const line of this.arrangement.chord_chart.split('\n')) {
+      // TODO: add all types of verses
+      if (line.startsWith('Verse') || line.startsWith('Chorus') || line.startsWith('Tag')) {
+        this.lyricVerses[currentVerseName] = currentVerse;
+        currentVerseName = line.trim();
+        currentVerse = '';
+      } else {
+        currentVerse += line + '\n';
+      }
+    }
+    if (currentVerse.length > 0) {
+      this.lyricVerses[currentVerseName] = currentVerse;
+    }
   }
 
+  raw_lyric_size(lyrics: string): number {
+    return lyrics.split('\n').length;
+  }
 }
